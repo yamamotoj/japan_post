432fbdcc4158685609d77e675a4f7d8efc105d9c
j.yamamoto <jumpei.yamamoto@gmail.com>
2018-07-18 16:17:17 +0900

    save data with splitting

--- japan_post/ken_all_address_parser.py
+++ japan_post/ken_all_address_parser.py
@@ -1,7 +1,7 @@
 import sqlite3
 from pathlib import Path
 
-from japan_post import ken_all
+from japan_post import ken_all, ken_all_address_splitter
 
 
 class KenAllAddressParser:
@@ -32,12 +32,10 @@ class KenAllAddressParser:
              jis_code TEXT NOT NULL ,
              old_zip_code TEXT NOT NULL ,
              zip_code TEXT NOT NULL ,
-             prefecture_name_kana TEXT NOT NULL ,
-             shikuchoson_name_kana TEXT NOT NULL ,
-             choiki_name_kana TEXT NOT NULL ,
-             prefecture_name TEXT NOT NULL ,
-             shikuchoson_name TEXT NOT NULL ,
-             choiki_name TEXT NOT NULL ,
+             prefecture TEXT NOT NULL ,
+             shikuchoson TEXT NOT NULL ,
+             choiki1 TEXT NOT NULL ,
+             choiki2 TEXT NOT NULL ,
              is_multipule_zip_code INTEGER NOT NULL ,
              has_koaza INTEGER NOT NULL ,
              has_chome INTEGER NOT NULL ,
@@ -46,13 +44,21 @@ class KenAllAddressParser:
              reason INTEGER  NOT NULL
             );
             """)
+        cursor = connection.cursor()
         for r in ken_all.concatinate_read(csv_path):
-            cursor = connection.cursor()
-            cursor.execute(
-                """
-                INSERT INTO ken_all(jis_code, old_zip_code, zip_code, prefecture_name_kana,
-                 shikuchoson_name_kana, choiki_name_kana, prefecture_name, shikuchoson_name,
-                 choiki_name, is_multipule_zip_code, has_koaza, has_chome, is_multiple_chome,
-                 has_update, reason)
-                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                """, r)
+            for choiki1, choiki2 in ken_all_address_splitter.split_choiki(r[8]):
+                cursor.execute(
+                    """
+                    INSERT INTO ken_all(jis_code, old_zip_code, zip_code,
+                    prefecture, shikuchoson,choiki1, choiki2,is_multipule_zip_code,
+                     has_koaza, has_chome, is_multiple_chome,has_update, reason)
+                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    """, (
+                        r[0], r[1], r[2], r[6], r[7], choiki1, choiki2, r[9], r[10], r[11], r[12],
+                        r[13], r[14]))
+
+        cursor.execute("CREATE INDEX prefecture_index ON ken_all(prefecture);")
+        cursor.execute("CREATE INDEX shikuchoson_index ON ken_all(shikuchoson);")
+        cursor.execute("CREATE INDEX choiki1_index ON ken_all(choiki1);")
+        cursor.execute("CREATE INDEX address_index ON ken_all(prefecture, shikuchoson, choiki1);")
+        cursor.execute("CREATE INDEX zip_code_index ON ken_all(zip_code);")
